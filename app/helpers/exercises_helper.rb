aa83c9afb77bf2705cc76591aecf2e1275474bbd
Shilcof <robert.shilcof@gmail.com>
2021-01-13 17:36:45 +0000

    adds muscle index page helper methods

--- app/helpers/exercises_helper.rb
+++ app/helpers/exercises_helper.rb
@@ -1,2 +1,13 @@
 module ExercisesHelper
+    def exercise_show_title
+        @muscle ? "Exercises for #{@muscle.name}" : "All exercises"
+    end
+
+    def new_exercise_link
+        if @muscle
+            button_to "Add a new exercise", new_muscle_exercise_path(@muscle), method: :get
+        else
+            button_to "Add a new exercise", new_exercise_path, method: :get
+        end
+    end
 end
