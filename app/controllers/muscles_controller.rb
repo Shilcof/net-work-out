a185283d236a870bd16df091f349f8bdd54a9969
Shilcof <robert.shilcof@gmail.com>
2021-01-13 15:05:34 +0000

    adds MusclesController actions

--- app/controllers/muscles_controller.rb
+++ app/controllers/muscles_controller.rb
@@ -2,31 +2,39 @@ class MusclesController < ApplicationController
     before_action :set_muscle, only: [:show, :edit, :update, :destroy]
 
     def index
-        
+        @muscles = Muscle.all
     end
     
     def new
-        
+        @muscle = Muscle.new
     end
     
     def create
-        
+        @muscle = Muscle.new(muscle_params)
+        if @muscle.save
+            redirect_to @muscle
+        else
+            render :new
+        end
     end
     
     def show
-        
     end
     
     def edit
-        
     end
     
     def update
-        
+        @muscle.update(muscle_params)
+        if @muscle.save
+            redirect_to @muscle
+        else
+            render :edit
+        end
     end
     
     def destroy
-        
+        @muscle.destroy
     end
 
     private
