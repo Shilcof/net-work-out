769fa675e0dd342dc7efad62d99645538f06fec2
Shilcof <robert.shilcof@gmail.com>
2021-01-13 14:56:10 +0000

    prepares MusclesController

--- app/controllers/muscles_controller.rb
+++ app/controllers/muscles_controller.rb
@@ -1,2 +1,41 @@
 class MusclesController < ApplicationController
+    before_action :set_muscle, only: [:show, :edit, :update, :destroy]
+
+    def index
+        
+    end
+    
+    def new
+        
+    end
+    
+    def create
+        
+    end
+    
+    def show
+        
+    end
+    
+    def edit
+        
+    end
+    
+    def update
+        
+    end
+    
+    def destroy
+        
+    end
+
+    private
+
+    def muscle_params
+        params.require(:muscle).permit(:name, :information)
+    end
+
+    def set_muscle
+        @muscle = Muscle.find(params[:id])
+    end
 end
