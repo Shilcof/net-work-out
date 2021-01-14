d4a1e5b1acc0d0ab370b8145ead87236e5ebce3c
Shilcof <robert.shilcof@gmail.com>
2021-01-14 12:56:08 +0000

    adds temporary rootpath

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
+  root 'exercises#index'
+
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   post 'logout', to: 'sessions#destroy'
-  
+
   resources :exercises
   resources :muscles do
     resources :exercises, only: [:index, :new]
