938ae1068e1c615a5b65b4b4eadc3638066325de
Shilcof <robert.shilcof@gmail.com>
2021-01-14 13:11:36 +0000

    adds SessionController action functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,23 @@
 class SessionsController < ApplicationController
+  skip_before_action :redirect_if_not_logged_in, only: [:new, :create]
+
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.find_by(username: params[:user][:username])
+    if @user && @user.authenticate(params[:user][:password])
+      login(@user)
+      redirect_to @user
+    else
+    @user = User.new
+    render :new
+    end
   end
 
   def destroy
+    reset_session
+    redirect_to root_path
   end
 end
