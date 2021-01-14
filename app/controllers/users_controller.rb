391c9d58b57148832fabdb40b47f4b804aa342f0
Shilcof <robert.shilcof@gmail.com>
2021-01-14 13:03:22 +0000

    implements sign up functionality and removes not logged in restriction from sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:show, :edit, :update, :destroy]
+    skip_before_action :redirect_if_not_logged_in, only: [:new, :create]
 
     def index # admin features
         @users = User.all
@@ -12,6 +13,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            login(@user)
             redirect_to @user
         else
             render :new
