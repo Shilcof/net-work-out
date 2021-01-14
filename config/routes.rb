8fa5c2b9081c568332c1fd0b9ba02540ee748b4d
Shilcof <robert.shilcof@gmail.com>
2021-01-14 12:50:21 +0000

    updates routes for SessionController

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  post 'logout', to: 'sessions#destroy'
+  
   resources :exercises
   resources :muscles do
     resources :exercises, only: [:index, :new]
