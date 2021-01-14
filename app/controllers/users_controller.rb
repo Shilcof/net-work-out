ebe8ce4b6746adb2f21b024e83701c8fed551dbc
Shilcof <robert.shilcof@gmail.com>
2021-01-14 12:33:39 +0000

    prepares UserController's actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,49 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+    def index # admin features
+        @users = User.all
+    end
+    
+    def new
+        @user = User.new
+    end
+    
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to @user
+        else
+            render :new
+        end
+    end
+    
+    def show
+    end
+    
+    def edit # require current user or admin check
+    end
+    
+    def update # require current user or admin check
+        @user.update(user_params)
+        if @user.save
+            redirect_to @user
+        else
+            render :edit
+        end
+    end
+    
+    def destroy # require current user or admin check -- delete dependant workouts, but not muscles & exercises
+        @user.destroy
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password, :password_confirmation, :admin)
+    end
+
+    def set_user
+        @user = User.find(params[:id])
+    end
 end
