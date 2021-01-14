874cc5248fc5e96945fc905700eadf9e73502090
Shilcof <robert.shilcof@gmail.com>
2021-01-14 12:46:20 +0000

    generated sessions controller files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   resources :exercises
   resources :muscles do
     resources :exercises, only: [:index, :new]
