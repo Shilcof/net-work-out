d9d115a13a2b9b04183a55d2e21d300f90da46f4
Shilcof <robert.shilcof@gmail.com>
2021-01-15 14:36:43 +0000

    updates user table to accomodate for omniauth log ins

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_13_171256) do
+ActiveRecord::Schema.define(version: 2021_01_15_143541) do
 
   create_table "exercises", force: :cascade do |t|
     t.string "name"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 2021_01_13_171256) do
     t.boolean "admin"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "uid"
+    t.string "provider"
   end
 
   create_table "workout_exercises", force: :cascade do |t|
