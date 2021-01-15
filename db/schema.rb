b75ad534a8532858a1237387723b816b84ad968b
Shilcof <robert.shilcof@gmail.com>
2021-01-15 16:28:49 +0000

    adds default admin false to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_15_143541) do
+ActiveRecord::Schema.define(version: 2021_01_15_162713) do
 
   create_table "exercises", force: :cascade do |t|
     t.string "name"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2021_01_15_143541) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "uid"
