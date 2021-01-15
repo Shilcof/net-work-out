2bcd9a28212599bd808bdf738394879e634e0144
Shilcof <robert.shilcof@gmail.com>
2021-01-15 14:41:48 +0000

    prepares user model to handel omniauth logins

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
     has_many :workouts
 
     has_secure_password
+
+    def self.from_omniauth(auth)
+        User.find_or_create_by(uid: auth[:uid], provider: auth[:provider]) do |u|
+            u.username = auth[:info][:name]
+            u.password = SecureRandom.hex(32)
+        end
+    end
 end
