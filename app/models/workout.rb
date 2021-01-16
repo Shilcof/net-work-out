6d58b06fcfa6988370a41acd6d619fef2d9242d3
Shilcof <robert.shilcof@gmail.com>
2021-01-16 14:47:13 +0000

    prepares workout show page and adds seed data into seed.rb file

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -2,6 +2,14 @@ class Workout < ApplicationRecord
   belongs_to :user
   has_many :workout_exercises
   has_many :exercises, through: :workout_exercises
-  
-  accepts_nested_attributes_for :workout_exercises
+
+  # accepts_nested_attributes_for :workout_exercises #write custom
+
+  def workout_exercises_attributes=(workout_exercises_attributes)
+    workout_exercises.each{ |we| we.destroy }
+    workout_exercises_attributes.each_with_index do |attributes, i|
+      attributes[exercise_number] = i
+      workout_exercises.each.create(attributes)
+    end
+  end
 end
