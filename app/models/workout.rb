b794d1131a49c23a151d33490f23c34304369247
Shilcof <robert.shilcof@gmail.com>
2021-01-17 10:59:48 +0000

    alters workout nested attribute writer to test for validity in order to render invalid fields

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -6,10 +6,11 @@ class Workout < ApplicationRecord
   # accepts_nested_attributes_for :workout_exercises #write custom
 
   def workout_exercises_attributes=(workout_exercises_attributes)
+    valid?
     workout_exercises.each{ |we| we.destroy }
     workout_exercises_attributes.each do |i, attributes|
       attributes[:exercise_number] = i.to_i
-      self.workout_exercises.build(attributes) if attributes[:exercise_id].present?
+      we = workout_exercises.build(attributes) if attributes[:exercise_id].present? && attributes[:sets].present? && attributes[:reps].present?
     end
   end
 end
