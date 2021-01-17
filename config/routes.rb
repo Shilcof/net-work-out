f38256cadb706e73811bf9fb9c4b61683a8e0efc
Shilcof <robert.shilcof@gmail.com>
2021-01-17 16:58:31 +0000

    adds workout routes for a specified user, and starred_workouts for future model changes

--- config/routes.rb
+++ config/routes.rb
@@ -7,18 +7,20 @@ Rails.application.routes.draw do
   post 'logout', to: 'sessions#destroy'
   get 'auth/:provider/callback', to: 'sessions#omniauth'
 
-  get 'signup', to: 'users#new'
-
   resources :exercises
   resources :muscles do
     resources :exercises, only: [:index, :new]
   end
-  resources :workout_exercises
   resources :workouts
+
+  get 'signup', to: 'users#new'
 
   resources :users, except: [:new, :show, :edit]
   get '/:username', to: 'users#show', as: :profile
   get '/:username/edit', to: 'users#edit', as: :edit_profile
+  
+  get '/:username/workouts', to: 'workouts#index', as: :my_workouts
+  get '/:username/starred_workouts', to: 'workouts#index', as: :starred_workouts
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
