8bc69246cde0b2967fcdc297729c51dfbe47d9d8
Shilcof <robert.shilcof@gmail.com>
2021-01-19 22:41:50 +0000

    alters spacing of formattable module

--- app/models/concerns/formattable.rb
+++ app/models/concerns/formattable.rb
@@ -1,9 +1,9 @@
 module Formattable
     def html
         if try(:information)
-            information.gsub(/\n/,'<br/>').html_safe
+            information.gsub(/[\n\r]+/,'<br/><br/>').html_safe
         else
-            bio.gsub(/\n/,'<br/>').html_safe
+            bio.gsub(/[\n\r]+/,'<br/><br/>').html_safe
         end
     end
 end
