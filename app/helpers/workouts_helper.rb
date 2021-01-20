029a33c851f3fe9c927f771d7ee5735848f657c8
Shilcof <robert.shilcof@gmail.com>
2021-01-20 12:41:55 +0000

    adds workout index page title helper

--- app/helpers/workouts_helper.rb
+++ app/helpers/workouts_helper.rb
@@ -1,2 +1,9 @@
 module WorkoutsHelper
+    def workout_index_title
+        if current_uri =~ /starred_workouts\z/
+            "Workouts starred by #{@user.username}"
+        else
+            @user ? "All workouts created by #{@user.username}" : "All workouts"
+        end
+    end
 end
